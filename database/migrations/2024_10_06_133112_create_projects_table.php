84be0dd5873bd9ba134db053618af930ccd2dfab
mariannacoppa <densimetra@gmail.com>
2024-10-06 15:38:10 +0200

    [UPD] inserted table variables and migration

--- database/migrations/2024_10_06_133112_create_projects_table.php
+++ database/migrations/2024_10_06_133112_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 200);
+            $table->string('slug');
+            $table->text('summary')->nullable();
             $table->timestamps();
         });
     }

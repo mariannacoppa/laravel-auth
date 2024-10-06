5a8f33362eae5a03da44da082de4f2dfb9ad97d4
mariannacoppa <densimetra@gmail.com>
2024-10-06 23:49:19 +0200

    [UPD] created function for generating slug

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'summary'];
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
 }

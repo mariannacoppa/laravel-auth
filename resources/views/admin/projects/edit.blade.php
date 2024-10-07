d5bee7146f531a833d226ae5b3f7928c8b49be82
mariannacoppa <densimetra@gmail.com>
2024-10-07 18:15:33 +0200

    [UPD] written edit operation

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.dashboard')
+
+@section('main-content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>Modifica un progetto</h2>
+        </div>
+        <div class="col-12">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="list-unstyled">
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <form action="{{ route('admin.projects.update', ['project' => $project->id]) }}" method="post">
+                @csrf
+                @method('PUT')
+                <div class="row">
+                    <div class="col-12">
+                        <label for="" class="control-label">Nome progetto</label>
+                        <input type="text" name="name" id="" class="form-control form-control-sm"
+                            placeholder="Nome pogetto">
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="control-label">Sommario progetto</label>
+                        <textarea name="summary" id="" cols="30" rows="10" class="form-control form-control-sm"
+                            placeholder="Nome pogetto"></textarea>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-sm btn-success">Salva</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection

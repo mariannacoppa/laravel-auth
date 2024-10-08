0743f226a7f109a47cee653f67249e3a9d5eb324
mariannacoppa <densimetra@gmail.com>
2024-10-08 17:14:50 +0200

    [UPD] changed default value in filesystems and .en files, created symlink, added enctype form attribute, added form field

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -39,6 +39,7 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
+        // $img_path = Storage::put('uploads', $data['image']);
         $form_data = $request->validated();
         $project = new Project();
         $form_data['slug'] = Project::generateSlug($form_data['name'], '-');
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -16,14 +16,14 @@
                 </ul>
             </div>
             @endif
-            <form action="{{ route('admin.projects.store') }}" method="post">
+            <form action="{{ route('admin.projects.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="row">
                     <div class="col-12">
                         <label for="" class="control-label">Nome progetto</label>
                         <input type="text" name="name" id=""
                             class="form-control form-control-sm @error('name') is-invalid @enderror"
-                            placeholder="Nome pogetto" value="{{ old('name') }}">
+                            placeholder="Nome pogetto" value="{{ old('name') }}" required>
                         @error('name')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -32,6 +32,10 @@
                         <label for="" class="control-label">Sommario progetto</label>
                         <textarea name="summary" id="" cols="30" rows="10" class="form-control form-control-sm"
                             placeholder="Nome pogetto">{{ old('summary') }}</textarea>
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="form-label">Inserisci immagine</label>
+                        <input class="form-control form-control-sm" type="file" id="cover_image" name="cover_image">
                     </div>
                     <div class="col-12">
                         <button type="submit" class="btn btn-sm btn-success">Salva</button>
